9ea31f93fd9011ef5537e9e09c02d4cbe18d93eb
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-02 21:40:46 -0800

    Comment out line

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleOpen() {
 function toggleActive(e) {
   // When panel is clicked and open class is added, there are two transition properties: font size and flex (flex-grow is applied using one-value flex shorthand property), but only care about flex-grow
   // Checking if it includes flex rather than if property is flex-grow because some browsers might interpret it as flex (Safari) while others consider it flex-grow
-  console.log(e.propertyName);
+  // console.log(e.propertyName);
   // includes() is used as a String prototype method here
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
